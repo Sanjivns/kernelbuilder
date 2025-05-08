fe4c1eaf7bdbcaa0140b3a8eb12572100b1a389e
Sanji <113604406+Sanjivns@users.noreply.github.com>
2025-05-09 00:34:56 +0600

    checkout latest

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/Sanjis-Android-Playground/kernel_sm8250-16.1.0.git --depth 1 kernel
+git clone https://github.com/Sanjis-Android-Playground/kernel_sm8250-16.1.0.git -b latest --depth 1 kernel
 cp -r kbuild.sh kernel/

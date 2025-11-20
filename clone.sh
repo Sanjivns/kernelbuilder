1f2cafd520ff38b73500cfd28915d609301e6522
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2025-11-20 05:13:00 +0000

    time to fix bt

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/Sanjivns/kernel_xiaomi_sm8250no.git --depth 1 kernel
+git clone https://github.com/EmanuelCN/kernel_xiaomi_sm8250.git -b bpf --depth 1 kernel
 cp -r kbuild.sh kernel/

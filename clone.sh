ec0c6040a211e55627a8129fa3d1667890dd3964
Sanji <113604406+Sanjivns@users.noreply.github.com>
2026-03-21 00:00:51 +0600

    Update kernel repository URL in clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/Sanjis-Android-Playground/kernel_sm8250.git --depth 1 -b fkv1.9 kernel
+git clone https://github.com/kvsnr113/xiaomi_sm8250_kernel_e404.git --depth 1 -b fkv1.9 kernel
 cp -r kbuild.sh kernel/

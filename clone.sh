6d69eccd77272ba238152ecdc771c6269543449e
Sanji <113604406+Sanjivns@users.noreply.github.com>
2026-03-21 00:03:59 +0600

    Remove branch specification from git clone command

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/kvsnr113/xiaomi_sm8250_kernel_e404.git --depth 1 -b fkv1.9 kernel
+git clone https://github.com/kvsnr113/xiaomi_sm8250_kernel_e404.git --depth 1 kernel
 cp -r kbuild.sh kernel/

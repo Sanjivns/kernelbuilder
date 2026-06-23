edade53909f12f44dfe86daea0a33fb1cdd27be6
Sanji <113604406+Sanjivns@users.noreply.github.com>
2026-06-23 12:35:47 +0600

    Clone kernel from staging-bpf branch

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/Sanjis-Android-Playground/android_kernel_xiaomi_sm8250.git --depth 1 kernel
+git clone https://github.com/Sanjis-Android-Playground/android_kernel_xiaomi_sm8250.git --depth 1 -b staging-bpf kernel
 cp -r kbuild.sh kernel/

d2accc11aa634cae6d12b93a8ed94925eb30657e
Sanji <113604406+Sanjivns@users.noreply.github.com>
2026-07-25 23:30:13 +0600

    Changed compiler from 'aosp' to 'nex14' and updated toolchain cloning commands.
    Update git clone branch for nex14 in kbuild.sh

--- kbuild.sh
+++ kbuild.sh
@@ -58,7 +58,7 @@ FINAL_ZIP=${ZIPNAME}-${DEVICE}-KERNEL-AOSP-${TM}.zip
 
 
 # Specify compiler [ proton, nexus, aosp ]
-COMPILER=aosp
+COMPILER=nex14
 
 # Clone ToolChain
 function cloneTC() {
@@ -89,7 +89,9 @@ function cloneTC() {
 			;;
 
 		nex14)
-			git clone --depth=1  https://gitlab.com/Project-Nexus/nexus-clang.git -b nexus-14 clang
+			git clone --depth=1  https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r596125.git -b clang
+            git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 gcc
+			git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git  --depth=1 gcc32
 			PATH="${KERNEL_DIR}/clang/bin:$PATH"
 			;;
 

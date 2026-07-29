d60c04d76206a035a0ab46c0429e008a0862eab7
Sanji <113604406+Sanjivns@users.noreply.github.com>
2026-07-29 11:33:53 +0600

    Change compiler to 'proton' and update toolchain URLs

--- kbuild.sh
+++ kbuild.sh
@@ -58,7 +58,7 @@ FINAL_ZIP=${ZIPNAME}-${DEVICE}-KERNEL-AOSP-${TM}.zip
 
 
 # Specify compiler [ proton, nexus, aosp ]
-COMPILER=aosp
+COMPILER=proton
 
 # Clone ToolChain
 function cloneTC() {
@@ -67,11 +67,13 @@ function cloneTC() {
 	
 		proton)
 			git clone --depth=1  https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r522817.git clang
+			git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 gcc
+			git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git  --depth=1 gcc32
 			PATH="${KERNEL_DIR}/clang/bin:$PATH"
 			;;
 		
 		nexus)
-			git clone --depth=1  https://gitlab.com/Project-Nexus/nexus-clang.git clang
+			git clone --depth=1  https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r596125.git clang
 			PATH="${KERNEL_DIR}/clang/bin:$PATH"
 			;;
 

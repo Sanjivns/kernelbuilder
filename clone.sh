5f56df88fe17c1549284566e8190dc95b9cb388f
Sanji <113604406+Sanjivns@users.noreply.github.com>
2026-07-29 11:39:23 +0600

    Update kernel repository URL in clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/Rve27/android_kernel_xiaomi_sm8250.git --depth 1 -b lineage-23.2-ksu-next kernel
+git clone https://github.com/Sanjis-Android-Playground/android_kernel_xiaomi_sm8250rv.git --depth 1 -b lineage-23.2-ksu-next kernel
 cp -r kbuild.sh kernel/

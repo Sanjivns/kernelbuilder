fbe09e42aac65566acc2777dd08f6d648f840c86
Sanji <113604406+Sanjivns@users.noreply.github.com>
2026-09-02 19:36:11 +0600

    Change branch to stable-bpf in clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/Sanjis-Android-Playground/kernel_sm8250.git --depth 1 -b stable kernel
+git clone https://github.com/Sanjis-Android-Playground/kernel_sm8250.git --depth 1 -b stable-bpf kernel
 cp -r kbuild.sh kernel/

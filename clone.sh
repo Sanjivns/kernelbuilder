d482e2b085ba92f90e1fc4f7e9e3e700caa692c6
Sanji <113604406+Sanjivns@users.noreply.github.com>
2026-09-02 16:36:58 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/Sanjis-Android-Playground/kernel_sm8250.git --depth 1 -b fkv1.9 kernel
+git clone https://github.com/Sanjis-Android-Playground/kernel_sm8250.git --depth 1 -b stable kernel
 cp -r kbuild.sh kernel/
